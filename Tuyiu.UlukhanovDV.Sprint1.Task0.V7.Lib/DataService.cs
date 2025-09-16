a1f5f9d0d4e480e7fcd49659907018aa99d20ae2
ulukhanovdzhabrail <djabrail192007@mail.ru>
2025-09-16 23:22:12 +0500

    Add project files.

---
+++ Tuyiu.UlukhanovDV.Sprint1.Task0.V7.Lib/DataService.cs
@@ -0,0 +1,12 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint1;
+
+namespace Tuyiu.UlukhanovDV.Sprint1.Task0.V7.Lib
+{
+    public class DataService : ISprint1Task0V7
+    {
+        public double Calculate()
+        {
+            return 18 / 6 * 2 - (4 - 3);
+        }
+    }
+}
---
+++ Tuyiu.UlukhanovDV.Sprint1.Task0.V7.Test/DataServiceTest.cs
@@ -0,0 +1,15 @@
+﻿using Tuyiu.UlukhanovDV.Sprint1.Task0.V7.Lib;
+namespace Tuyiu.UlukhanovDV.Sprint1.Task0.V7.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            var res = ds.Calculate();
+            Assert.AreEqual(5, res);
+        }
+    }
+}
