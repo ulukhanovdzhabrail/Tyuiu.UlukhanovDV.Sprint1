8a19791d02acb0ea96a494100b03f34c85f6ca18
ulukhanovdzhabrail <djabrail192007@mail.ru>
2025-09-29 15:45:20 +0500

    Add files via upload

---
+++ Tyuiu.UlukhanovDV.Sprint1.Task1.V14.Lib/DataService.cs
@@ -0,0 +1,12 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint1;
+namespace Tyuiu.UlukhanovDV.Sprint1.Task1.V14.Lib
+{
+    public class DataService : ISprint1Task1V14
+
+    {
+        public double Calculate(double a, double b, double c)
+        {
+            return a * b / c + (a / (b + c));
+        }
+    }
+}
---
+++ Tyuiu.UlukhanovDV.Sprint1.Task1.V14.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.UlukhanovDV.Sprint1.Task1.V14.Lib;
+namespace Tyuiu.UlukhanovDV.Sprint1.Task1.V14.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double a = 10.0;
+            double b = 10.0;
+            double c = 10.0;
+            var res = ds.Calculate(a, b, c);
+            Assert.AreEqual(10.5, res);
+
+        }
+    }
+}
